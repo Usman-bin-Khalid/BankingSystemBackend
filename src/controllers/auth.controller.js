1878c57b66d476c86db2271d851fd1316fafe9cf
Usman <usmanbinkhalidpk@gmail.com>
2026-05-19 05:50:09 +0500

    Register API Completed

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,41 @@
+const userModels = require('../models/user.model');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+
+
+async  function userRegisterController(req, res) {
+     const {email, password , name} = req.body;
+     const isExists = await userModels.findOne({
+        email : email,
+     });
+     if (isExists) {
+        return res.status(422).json({
+            message : 'User already exists with this email',
+            status : 'fail'
+        })
+     }
+
+     const user = await userModels.create({
+        email, password , name
+     });
+     const token = jwt.sign({
+        userId : user._id,
+     }, process.env.JWT_SECRET, {
+        expiresIn : "3d"
+     });
+     res.cookie("token" , token);
+     res.status(201).json({
+        user : {
+            _id : user._id,
+            email : user.email,
+            name : user.name
+        },
+        token : token
+     })
+
+}
+
+
+module.exports = {
+    userRegisterController
+}
