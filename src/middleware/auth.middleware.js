d76df02874b88e387270aa87fa1d577fde8fc16d
Usman <usmanbinkhalidpk@gmail.com>
2026-05-28 11:42:01 +0500

    Black List Token Add in Middleware

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,11 +1,16 @@
 const userModel = require('../models/user.model');
 const jwt = require('jsonwebtoken');
+const tokenBlackListModel = require('../models/blackList.model');
 
 
 async function authMiddleware(req, res, next) {
    const token = req.cookies?.token || req.headers.authorization?.split(' ')[1];
    if (!token) {
       return res.status(401).json({ message: 'Unauthorized, token is missing' });
+   }
+   const isBlackListed = await tokenBlackListModel.findOne({token});
+   if (isBlackListed) {
+      return res.status(401).json({message : 'Unauthorized access, token is invalid'});
    }
    try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
@@ -23,6 +28,10 @@ async function authSystemUserMiddleware(req, res, next) {
    const token = req.cookies?.token || re.headers.authorization?.split(' ')[1];
    if (!token) {
       return res.status(401).json({ message: 'Unauthorized, token is missing' });
+   }
+     const isBlackListed = await tokenBlackListModel.findOne({token});
+   if (isBlackListed) {
+      return res.status(401).json({message : 'Unauthorized access, token is invalid'});
    }
 
    try {
